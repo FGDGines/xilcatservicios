5bad1a6c83e1bc5f490985ea3d9307a38a10cc12
FGDlexx54 <lexington@fgddesarrolloweb.es>
2023-11-15 15:46:27 -0400

    add few styles

--- front/src/components/Landing/AboutUs.tsx
+++ front/src/components/Landing/AboutUs.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <div className='min-h-[20vh] p-2'>
-      <p className='text-center text-2xl mb-8'>Sobre Nosotros</p>
+      <p className='text-center text-3xl mb-8 font bold'>Sobre Nosotros</p>
       <div className='flex flex-col md:flex-row gap-4'>
         <p className='text-center flex-1'>Llevamos más de 15 años asesorando y mejorando la calidad de vida de muchos extranjeros que llegan a España. En nuestra Web encontrarás algunas de las respuestas que necesitas para resolver algunos temas básicos de extranjería. </p>
         <div className='h-[400px] bg-black flex-1'></div>
