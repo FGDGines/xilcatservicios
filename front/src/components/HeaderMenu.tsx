b819927a861d7a6ce18994a3559a76f8c6986cd6
FGDlexx54 <lexington@fgddesarrolloweb.es>
2023-11-17 14:31:28 -0400

    improve z-index

--- front/src/components/HeaderMenu.tsx
+++ front/src/components/HeaderMenu.tsx
@@ -4,7 +4,7 @@ import { IoMenu } from "react-icons/io5";
 
 const HeaderMenu = () => {
     return (
-      <div className='sticky w-full h-16 bg-[#2C2949] left-0 top-0 flex justify-between items-center px-4'>
+      <div className='sticky w-full h-16 bg-[#2C2949] left-0 top-0 flex justify-between items-center px-4 z-50'>
         <div>
             <img src={Logo}/>
         </div>
