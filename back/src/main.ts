ee13ba3e392fa9cccb535bdb7d5ace853e4aca7c
Pedro Alberto Quispe Colca <pedro@fgddesarrolloweb.es>
2023-11-23 14:24:25 -0500

    CORS

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: true, // Permite solicitudes desde todas las fuentes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Accept', // Encabezados permitidos
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
 
   const config = new DocumentBuilder()
     .setTitle('Nombre de tu API')
@@ -14,6 +23,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors(corsOptions); // Habilitar CORS con las opciones especificadas
 
   await app.listen(3000);
 }
