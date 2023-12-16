5e896d5320197fa395b9dd58e3a6ba55ccd60936
FGDlexx54 <lexington@fgddesarrolloweb.es>
2023-12-16 17:32:02 -0400

    agrega estilo basico del card de blog

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [ './src/**/*.{js,jsx,ts,tsx}',],
   theme: {
     extend: {
+      content: {
+        'logo': 'url("./src/assets/Logo_white.png")'
+      },
       colors: {
         'cs-purple': '#2C2949',
         'cs-gray': '#252323'
